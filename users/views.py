0714f335217622ec02baba3ee9ea105486758cbc
yoons <kys5037k@naver.com>
2022-07-26 22:41:22 +0900

    final

--- users/views.py
+++ users/views.py
@@ -1,9 +1,23 @@
 from django.shortcuts import render
-from main.models import Post
 from django.contrib.auth.models import User
+from main.models import *
 
 def mypage(request):
     user=request.user
     #로그인한 유저이름과 글 작성자 이름이 동일한 글 필터링
+
     posts=Post.objects.filter(writer=user)
-    return render(request,'users/mypage.html',{'posts':posts})
+    like_list = Like.objects.filter(user = user)
+    dislike_list = Dislike.objects.filter(user = user)
+
+    context = {
+        'like_list' : like_list,
+        'posts':posts,
+    }
+
+    context = {
+        'dislike_list' : dislike_list,
+        'posts':posts,
+    }
+    return render(request,'users/mypage.html',context)
+
